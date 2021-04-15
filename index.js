baaa14736a662e9cf98c2b3df5856f39248f6d31
DG <dg@email.com>
2021-04-15 12:56:52 +0530

    add market tracing capability in bot

--- index.js
+++ index.js
@@ -5,65 +5,49 @@ const axios = require("axios");
 const tick = async (config, binanceClient) => {
   const { asset, base, spread, allocation } = config;
   const market = `${asset}/${base}`;
+  const timeslug = new Date();
 
-  // Cancel open orders left from previou tick, if any
-  const orders = await binanceClient.fetchOpenOrders(market);
-  orders.forEach(async order => {
-    await binanceClient.cancelOrder(order.id,market)
-  });
-
-  // Fetch current market prices
-  //https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cbusd&vs_currencies=usd
-  const results = await Promise.all([
-    axios.get('https://api.coingecko.com/api/v3/simple/price?ids=chiliz&vs_currencies=usd'),
-    axios.get('https://api.coingecko.com/api/v3/simple/price?ids=tether&vs_currencies=usd')
-  ]);
-  const marketPrice = results[0].data.chiliz.usd / results[1].data.tether.usd;
-
-  // Calculate new orders parameters
-  const sellPrice = marketPrice * (1 + spread);
-  const buyPrice = marketPrice * (1 - spread);
+  // get balance of base coin
   const balances = await binanceClient.fetchBalance();
-  const assetBalance = balances.free[asset]; // e.g. 0.01 BTC
-  const baseBalance = balances.free[base]; // e.g. 20 USDT
-  const sellVolume = assetBalance * allocation;
-  const buyVolume = (baseBalance * allocation) / marketPrice;
+  const baseBalance = balances.free[base];
+  console.log("Base Balance for ",market, baseBalance)
 
-  const fullMarket = await binanceClient.loadMarkets();
-  console.log("**** Market ",fullMarket[market]['limits']['cost']['min'])
-  
-  console.log(`
-  ******************************************************
-    Starting Balances...
-    BASE = ${baseBalance}
-    ASSET = ${assetBalance}
-    Market Price....
-    ${marketPrice}
-    Buy Cost...
-    ${buyVolume * buyPrice}
-    Sell Cost...
-    ${sellVolume * sellPrice}
-  `);
+  const fullMarket = await binanceClient.fetchTicker(market);
+  // console.log("**** Market ",fullMarket[market]['limits']['cost']['min'])
+  // console.log("**** Market ",fullMarket )
 
-  //Send orders
-  await binanceClient.createLimitSellOrder(market, sellVolume, sellPrice);
-  await binanceClient.createLimitBuyOrder(market, buyVolume, buyPrice);
-
-  console.log(`
-    New tick for ${market}...
-    Created limit sell order for ${sellVolume}@${sellPrice}  
-    Created limit buy order for ${buyVolume}@${buyPrice}  
-  `);
+  //if base coin available then create a market buy and limit sell order
+  if (baseBalance >= allocation) {
+    //Send orders
+    const buyPrice = allocation / fullMarket.ask; // buy on current market price
+    binanceClient.createMarketBuyOrder(market, buyPrice).then((result) => {
+      console.log(`- ${timeslug} - Created market buy order for '${market}' with ${result.amount}@${result.price}`)
+      const sellVolume = result.amount;
+      const sellPrice = result.price + (result.price * spread / 100); //this is the profit based on spread percentage
+      binanceClient.createLimitSellOrder(market, sellVolume, sellPrice).then((sellResult) => {
+        console.log(`- ${timeslug} - Created limit sell order for '${market}' with ${sellResult.amount}@${sellResult.price}`)
+      });
+    })
+  }
 };
 
 const run = () => {
-  const config = { 
-    asset: "CHZ",
-    base: "USDT",
-    allocation: 1,     // Percentage of our available funds that we trade
-    spread: 0.01,         // Percentage above and below market prices for sell and buy orders 
-    tickInterval: 30000//2000  // Duration between each tick, in milliseconds
-  };
+  const config = [
+    {
+      asset: "BTC", // this is the target coin
+      base: "BUSD", // this is our base coin 
+      allocation: 200,     // amount in base currency for which market buy order will be placed
+      spread: 0.8,         // Percentage to book profit, based on which limit sell order will be placed
+      tickInterval: 30000//2000  // Duration between each tick, in milliseconds
+    },
+    {
+      asset: "BNB",
+      base: "USDT",
+      allocation: 200,     // amount in base currency for which market buy order will be placed
+      spread: 0.8,         // Percentage to book profit, based on which limit sell order will be placed
+      tickInterval: 30000//2000  // Duration between each tick, in milliseconds
+    }
+  ];
   const binanceClient = new ccxt.binance({
     apiKey: process.env.APIKEY,
     secret: process.env.SECRETKEY
@@ -72,11 +56,13 @@ const run = () => {
   // TEST CONFIG BEGINS
   // https://testnet.binance.vision/
   // https://testnet.binance.org/en/trade/BUSD-227_BNB
-  // binanceClient.set_sandbox_mode(true)
+  binanceClient.set_sandbox_mode(true)
   // TEST CONFIG ENDS
-  
-  tick(config, binanceClient);
-  setInterval(tick, config.tickInterval, config, binanceClient);
+
+  config.map((item) => {
+    tick(item, binanceClient);
+    setInterval(tick, item.tickInterval, item, binanceClient);
+  });
 };
 
 run();
