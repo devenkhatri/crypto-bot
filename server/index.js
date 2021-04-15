9bc2b825a970527339d3564f51e8d310caf14f43
DG <dg@email.com>
2021-04-15 17:24:32 +0530

    corrected few configs

--- server/index.js
+++ server/index.js
@@ -5,11 +5,18 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-const buildPath = path.join(__dirname, '..', 'build');
+const buildPath = path.join(__dirname, '..', '/client/build');
+// console.log("**** buildPath", buildPath)
 app.use(express.static(buildPath));
 
 app.get("/status", (req, res) => {
     res.json({ message: "Hello from server!" });
+});
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', '/client/build/index.html'));
 });
 
 app.listen(PORT, () => {
