1a94e8da5a555aa790457dc68a05a9713c98dbf6
DG <dg@email.com>
2021-04-15 16:30:35 +0530

    changed serve the index.html file from the build folder and not the public folder.

--- server/index.js
+++ server/index.js
@@ -4,6 +4,9 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+const buildPath = path.join(__dirname, '..', 'build');
+app.use(express.static(buildPath));
+
 app.get("/status", (req, res) => {
     res.json({ message: "Hello from server!" });
 });
